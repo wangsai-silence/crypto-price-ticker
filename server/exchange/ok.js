d916361aeb542e97873dff702eba986ce62772e0
xfg <willcn@foxmail.com>
2025-01-14 10:04:44 +0800

    feature: update okex api

--- server/exchange/ok.js
+++ server/exchange/ok.js
@@ -1,15 +1,15 @@
 const rp = require('request-promise');
 
 function getAllSymbols() {
-    return rp.get('https://www.okex.com/api/spot/v3/instruments').
-                then(JSON.parse).
-                then((data) => data.map((a) => a.instrument_id).sort());
+    return rp.get('https://www.okex.com/api/v5/public/instruments?instType=SPOT').
+        then(JSON.parse).
+        then((data) => data.map((a) => a.instId).sort());
 }
 
 function getPrice(symbol) {
-    return rp.get(`https://www.okex.com/api/spot/v3/instruments/${symbol}/ticker`).
-                then(JSON.parse).
-                then((data) => data.last);
+    return rp.get(`https://www.okex.com/api/v5/market/index-tickers?instId=${symbol}`).
+        then(JSON.parse).
+        then((data) => data.last);
 }
 
 module.exports = {
